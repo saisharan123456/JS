fe36ff17447ca67c509402dbee1731f2df6beaf0
Sai Sharan <88391021+saisharan123456@users.noreply.github.com>
2024-10-02 02:12:28 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,4 @@
+const accountId = 144353
+let accountEmail= "saisharan794@gmail.com"
+var accountpswd="12345"
+city="jaipur"
---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,21 @@
+"use strict";//treat alljs code as newer version
+
+// alert("hello") we are using node js not browser
+
+// code readability should be high
+//number
+//sting
+//null => standalone value
+//boolean
+//undefined
+//bigint
+
+//symbol => unique
+
+//object =>
+
+
+console.log(typeof 0)
+
+console.log(typeof null) //object  but undefined is undefined itself. ***** IMPORTANT ******
+
