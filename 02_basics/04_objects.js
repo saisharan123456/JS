504d44523f7afa5d44f0fbc3d41cb24d0fb00cad
saisharan123456 <saisharan794@gmail.com>
2024-10-27 15:30:45 +0530

    api learning

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,5 @@
 // const tinderUser = new Object() -- singleton object
-
+ 
 const tinderUser = {}  // nonsingleton object
 
 tinderUser.id = "123.ssm"
@@ -7,7 +7,7 @@ tinderUser.name="Sammy"
 tinderUser.isloggedIn = "false"
 
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
 
 
@@ -18,18 +18,33 @@ const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"a",3:"b"}
 
 const obj3 = {obj1 ,obj2}
-console.log(obj3);   //now the same problem arises that had happend for arrays that is it becomes a nested array...we do not want that
+// console.log(obj3);   //now the same problem arises that had happend for arrays that is it becomes a nested array...we do not want that
 
 const obj4 = Object.assign({},obj1,obj2)  //this also we use less
-console.log(obj4);
+// console.log(obj4);
 
 const obj5= {...obj1,...obj2}
-console.log(obj5);
+// console.log(obj5);
 
 
-console.log(Object.keys(tinderUser)) // important -- the returned data type is a array
-console.log(Object.values(tinderUser)) 
-console.log(Object.entries(tinderUser)) 
+// console.log(Object.keys(tinderUser)) // important -- the returned data type is a array
+// console.log(Object.values(tinderUser)) 
+// console.log(Object.entries(tinderUser)) 
 
 
-console.log(tinderUser.hasOwnProperty('IsLoggedIn'))
+// console.log(tinderUser.hasOwnProperty('IsLoggedIn'))
+
+
+
+
+// ------------------------
+
+const couuse ={
+    coursename :"javascript",
+    price : "1000",
+    courseteacher:"hithesh"
+
+}
+
+
+console.log(couuse.courseteacher())
