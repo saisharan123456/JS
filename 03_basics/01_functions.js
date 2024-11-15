f10bb1ef0a4725ff52322374cf264dcaef17de05
saisharan123456 <saisharan794@gmail.com>
2024-11-16 03:02:17 +0530

    functions new

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,5 +1,40 @@
-function sum(a,b){
-    console.log(a+b);
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+// sum(1,8); 
+
+
+// function loggingMsgUser(name){
+//     if(name === undefined){
+//         console.log("give a  name ");
+        
+
+//     }
+//     else{
+//         return `${name} just logged in`}
+// }
+
+// a=loggingMsgUser();
+// console.log(a)
+
+
+
+// function bill(...num){
+//     return num
+// }
+
+// console.log(bill(1,2,3))
+
+
+const user = {
+    usernm : "Sai Sharan",
+    age : 18 ,
 }
 
-sum(1,4); 
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.usernm} and age is ${anyobject.age}`);
+    
+}
+
+handleObject(user)
