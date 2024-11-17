c84aa1dca3e0c1c37ec9384e7b573230a80c7985
saisharan123456 <saisharan794@gmail.com>
2024-11-17 19:46:59 +0530

    if else bascis

---
+++ 04_control_flow/one.js
@@ -0,0 +1,25 @@
+//  if
+
+// if (condition){
+
+// }
+
+// < , > ,<= ,>= , ==,!=,===(checks the dataType also)
+ 
+
+// if (2 === '2'){
+//     console.log("executed");
+// }
+// else{
+//     console.log("not")
+// }
+
+
+//  scope is very important in conditional  statemets
+
+// const balance = 1000
+
+// if (balance> 50)console.log("test");
+
+// and === &&
+// or  === ||
